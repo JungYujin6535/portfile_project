0e183209e1d7115b07f4ab8de951fcd6820cccc0
JungYujin6535 <dayouin2@naver.com>
2022-05-30 01:19:48 +0900

    Initial commit

---
+++ src/test/java/profile/portfile_project/PortfileProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package profile.portfile_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfileProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
